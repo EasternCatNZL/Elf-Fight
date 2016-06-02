1ef238ec713769c333567b496f62135bbc5bd45f
Casey <chimpsterchip@gmail.com>
2016-06-02 13:49:52 +1200

    Merge Projects 2# - 02/06/2016
    Second Attempt at merging the projects

---
+++ Elf Club 2D/Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour {
+
+    public Rigidbody2D weapon; //Rigidbody physics component of the weapon
+    public float attackSpeed; //The speed at which the weapon initially moves
+    [HideInInspector]
+    public Transform weaponTrans; //Transform of the weapon    
+    private Quaternion weaponRotate; //Rotation of the weapon
+
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void FixedUpdate()
+    {
+        Attack();
+    }
+
+    void Attack()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //Gets the hidden rotation of the player
+            weaponRotate = GetComponent<PlayerMovement>().rotate;
+            //Creates weapon using current transform and hidden rotation
+            Rigidbody2D weaponInstance = Instantiate(weapon, transform.position, weaponRotate) as Rigidbody2D;
+            //Gives weapon speed
+            //weaponInstance.velocity = attackSpeed * weaponInstance.transform.right ;
+        }
+    }
+}
